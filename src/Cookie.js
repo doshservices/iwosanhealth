816575843fb2f59e63dc468b99b0fbe4a6c6bfc5
prosper edafe <mamusedafe19@gmail.com>
2022-12-13 21:26:27 -0800

    added cookies

--- src/Cookie.js
+++ src/Cookie.js
@@ -11,12 +11,11 @@ const Cookies = () => {
                     <div>
                         <div className="cookie__text">
                             <h2>This site uses cookies</h2>
-                            <p>We use cookies on our website to give you the most relevant experience by remembering your preferences and repeat visits. By clicking "Accept All", you consent to the use of ALL the cookies. However, you may visit "Cookie Settings" to provide a controlled consent.
+                            <p>
+                                We use cookies on our website to give you the most relevant experience by remembering your preferences and repeat visits. By clicking "Accept All", you consent to the use of ALL the cookies. However, you may visit "Cookie Settings" to provide a controlled consent.
                             </p>
                         </div>
-
                         <button onClick={() => setClose(!close)} className="cookie__btn">Accept All</button>
-
                     </div>
                 </div>
             )}
