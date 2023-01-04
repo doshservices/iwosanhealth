2dc3aafc0ecf6b7e729624d32c59cba7390eeecf
prosper edafe <mamusedafe19@gmail.com>
2023-01-04 23:41:38 +0100

    swiper implemented

--- src/components/management/manage.jsx
+++ src/components/management/manage.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useState, useRef, useEffect } from "react";
 import management from "./data";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper";
+import "swiper/css";
+import "swiper/css/free-mode";
 
 const Management = () => {
   const scroll = ["<", ">"];
@@ -26,45 +30,84 @@ const Management = () => {
     };
   });
 
-  const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
+  // const nextSlide = () => {
+  //   setCurrent(current === length - 1 ? 0 : current + 1);
+  // };
+  // const prevSlide = () => {
+  //   setCurrent(current === 0 ? length - 1 : current - 1);
+  // };
+
+  // if (!Array.isArray(management) || management.length <= 0) {
+  //   return null;
+  // }
+  const slideLeft = () => {
+    let slider = document.getElementById("data-carousel");
+    slider.scrollLeft = slider.scrollLeft - 800;
   };
 
-  if (!Array.isArray(management) || management.length <= 0) {
-    return null;
-  }
+  const slideRight = () => {
+    let slider = document.getElementById("data-carousel");
+    slider.scrollLeft = slider.scrollLeft + 800;
+  };
 
   return (
     <>
-      <div className="board-carousel">
+      <Swiper
+        freeMode={true}
+        grabCursor={false}
+        modules={[FreeMode]}
+        slidesPerView={5}
+        spaceBetween={30}
+        className="board-carousel"
+        id="data-carousel"
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          600: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          950: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+          1250: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
+          1690: {
+            slidesPerView: 5,
+            spaceBetween: 10,
+          },
+        }}
+      >
         {management.map((data, index) => {
           return (
-            <div
+            <SwiperSlide
               key={data.id}
-              className={index === current ? "animate true" : "animate"}
+              // className={index === current ? "animate true" : "animate"}
             >
-              {index === current && (
-                <>
-                  <div
-                    onClick={(e) => handleClick(e, data)}
-                    className="staff"
-                    key={data.id}
-                  >
-                    <figure>
-                      <img src={data.img} alt={data.name} />
-                    </figure>
-                    <div>
-                      <h3>{data.name}</h3>
-                      <h4>{data.office}</h4>
-                      <span></span>
-                    </div>
+              {/* {index === current && ( */}
+              <>
+                <div
+                  onClick={(e) => handleClick(e, data)}
+                  className="staff"
+                  key={data.id}
+                >
+                  <figure>
+                    <img src={data.img} alt={data.name} />
+                  </figure>
+                  <div>
+                    <h3>{data.name}</h3>
+                    <h4>{data.office}</h4>
+                    <span></span>
                   </div>
-                </>
-              )}
-            </div>
+                </div>
+              </>
+              {/* )} */}
+            </SwiperSlide>
           );
         })}
         {showModal && (
@@ -93,12 +136,12 @@ const Management = () => {
             </div>
           </div>
         )}
-      </div>
+      </Swiper>
       <div className="scroller">
-        <button onClick={prevSlide} className="scroller">
+        <button onClick={slideLeft} className="scroller">
           {scroll[0]}
         </button>
-        <button onClick={nextSlide} className="scroller">
+        <button onClick={slideRight} className="scroller">
           {scroll[1]}
         </button>
       </div>
