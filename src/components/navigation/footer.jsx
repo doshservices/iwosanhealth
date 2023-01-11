d9122d4a95591ad391440187df1bbc4611fc582d
prosper edafe <mamusedafe19@gmail.com>
2023-01-11 12:06:46 +0100

    changed footer text

--- src/components/navigation/footer.jsx
+++ src/components/navigation/footer.jsx
@@ -16,10 +16,8 @@ const Footer = () => {
               <img className="footer__logo" src={Logo} alt="" />
             </Link>
             <p>
-              Iwosan Investments Limited is a privately held healthcare asset
-              and investment company dedicated to financing and developing
-              brownfield and greenfield multi-specialist healthcare facilities
-              in West Africa.
+              Iwosan Investments is a healthcare investment company dedicated to
+              investing in the Nigerian healthcare market.
             </p>
           </div>
           <nav>
